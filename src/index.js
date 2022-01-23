9babe5c09172bbabdd8b0e9cfad97f59c006282b
zhangjiepeng <zhangjiepeng@bytedance.com>
2022-01-23 15:32:40 +0800

    Change title to 'JSON Tools'

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+document.title = "JSON Tools";
 ReactDOM.render(
   <React.StrictMode>
     <App />
